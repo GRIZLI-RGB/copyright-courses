24cfb8630e3f60abda9e4aa724300799a1463d59
Evgeniy Kozlov <ek916569@gmail.com>
2024-01-06 22:31:54 +0500

    backend clearing

--- app/(pages)/admin-panel/layout.tsx
+++
@@ -1,9 +0,0 @@
-import type { Metadata } from "next";
-
-export const metadata: Metadata = {
-	title: "Админ-панель «Сиди, пиши»",
-};
-
-export default function AdminPanelLayout() {
-	return <></>;
-}
--- app/_components/Landing/Title.tsx
+++
@@ -1,7 +0,0 @@
-export default function Title({ children }: { children: React.ReactNode }) {
-	return (
-		<h2 className="font-bold text-[48px] leading-[120%] text-center">
-			{children}
-		</h2>
-	);
-}
